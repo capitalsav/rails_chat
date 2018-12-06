ab9e055117cebd3bc35a4ee56238bf3cf3065950
Anton <capitalsav@gmail.com>
2018-12-06 17:26:22 +0200

    Write api controller for contacts list

--- app/javascript/components/ContactsMenu.js
+++ app/javascript/components/ContactsMenu.js
@@ -1,17 +1,44 @@
 import React from 'react'
+import axios from 'axios'
 
 class ContactsMenu extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            contacts: this.props.contacts
-        };
 
-        // this.updateMessages = this.updateMessages.bind(this);
+        // this.state = {
+        //     contacts: ''
+        // };
+
+        this.componentDidMount = this.componentDidMount.bind(this);
     }
+
+    componentDidMount() {
+        axios.get('/api/v1/contacts', {
+
+        })
+        .then(function (response) {
+            console.log(response);
+            this.setState({
+                contacts : response.data
+            });
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        // console.log(this.state);
+
+    }
+
     render() {
-        console.log(this.state.contacts)
-        return <h1>{this.state.contacts}</h1>
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="submit" value="Submit" />
+            </form>
+        );
     }
 }
 
