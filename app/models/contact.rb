eb9bb39a7a3e836d94782a07411f9399ac54760e
Anton <capitalsav@gmail.com>
2018-12-14 16:24:35 +0200

    Move find contacts list into separated method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,10 @@
 class Contact < ApplicationRecord
   belongs_to :user, class_name: 'User'
   belongs_to :contact, class_name: 'User'
+
+  def self.current_user_contacts(user)
+    user.users.map { |x| User.find_by(id: x.contact_id) } +
+        user.contacts.map { |x| User.find_by(id: x.user_id) }
+  end
 end
 
