25aeb91b6b52ffe98fb05fedbb5fd728f0cc01ad
Anton <capitalsav@gmail.com>
2019-04-10 18:16:01 +0300

    Remove user_id from contacts API index json response

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,8 +3,8 @@ class Contact < ApplicationRecord
   belongs_to :contact, class_name: 'User'
 
   def self.current_user_contacts(user)
-    user.users.map { |x| User.find_by(id: x.contact_id) } +
-        user.contacts.map { |x| User.find_by(id: x.user_id) }
+    user.users.map { |x| User.where(id: x.contact_id).select('email', 'avatar', 'first_name', 'last_name', 'display_name') } +
+        user.contacts.map { |x| User.where(id: x.user_id).select('email', 'avatar', 'first_name', 'last_name', 'display_name')  }
   end
 end
 
