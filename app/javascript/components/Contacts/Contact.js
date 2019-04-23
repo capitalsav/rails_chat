92bce9fb67238408a99d953c0da8f22645ecbd2c
Anton <capitalsav@gmail.com>
2019-04-23 17:36:08 +0300

    Put avatar in the same line as display_name

--- app/javascript/components/Contacts/Contact.js
+++ app/javascript/components/Contacts/Contact.js
@@ -4,9 +4,15 @@ import axios from "axios";
 class Contact extends React.Component {
     render() {
         return (
-            <div>
-                <img src={this.props.user.avatar}/>
-                <div>{this.props.user.display_name}</div>
+            <div className="user-item">
+                <div className="row">
+                    <div className="col-3">
+                        <img src={this.props.user.avatar} className="user-avatar"/>
+                    </div>
+                    <div className="col-8">
+                        <div className="user-display-name">{this.props.user.display_name}</div>
+                    </div>
+                </div>
             </div>
         )
     }
