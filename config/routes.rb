476bc439d07404abb9dc80c40229008c9125087f
Anton <capitalsav@gmail.com>
2019-10-23 17:49:07 +0300

    Remove old routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  resources :chat_rooms, only: [:new, :create, :show, :index]
-  resources :private_chat_rooms, except: [:update, :edit, :destroy]
-  resources :multi_user_private_chat_rooms
-  resources :multi_user_memberships
   mount ActionCable.server => '/cable'
   root 'chat_rooms#index'
 end
