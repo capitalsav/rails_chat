a158346a4a1858551b30c58f850df6913f0ad7df
Anton <capitalsav@gmail.com>
2019-10-23 17:27:43 +0300

    Remove messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191023141442) do
+ActiveRecord::Schema.define(version: 20191023142443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "messages", force: :cascade do |t|
-    t.text "body"
-    t.bigint "user_id"
-    t.bigint "chat_room_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["chat_room_id"], name: "index_messages_on_chat_room_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
-  end
 
   create_table "private_chat_rooms", force: :cascade do |t|
     t.string "name"
@@ -56,5 +46,4 @@ ActiveRecord::Schema.define(version: 20191023141442) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "messages", "users"
 end
