f1474cc47379528736fd068e447ee8e0888731d2
Anton <capitalsav@users.noreply.github.com>
2023-09-11 17:58:49 +0300

    Run local migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_24_105833) do
-
+ActiveRecord::Schema[7.0].define(version: 2018_05_24_105833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "chat_rooms", force: :cascade do |t|
     t.string "title"
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id"], name: "index_chat_rooms_on_user_id"
   end
 
@@ -36,8 +35,8 @@ ActiveRecord::Schema.define(version: 2018_05_24_105833) do
     t.text "body"
     t.bigint "user_id"
     t.bigint "chat_room_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["chat_room_id"], name: "index_messages_on_chat_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
@@ -45,8 +44,8 @@ ActiveRecord::Schema.define(version: 2018_05_24_105833) do
   create_table "multi_user_memberships", force: :cascade do |t|
     t.bigint "multi_user_private_chat_room_id"
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["multi_user_private_chat_room_id"], name: "index_multi_user_memberships_on_multi_user_private_chat_room_id"
     t.index ["user_id"], name: "index_multi_user_memberships_on_user_id"
   end
@@ -55,8 +54,8 @@ ActiveRecord::Schema.define(version: 2018_05_24_105833) do
     t.text "content"
     t.bigint "user_id"
     t.bigint "multi_user_private_chat_room_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["multi_user_private_chat_room_id"], name: "index_multi_user_messages_on_multi_user_private_chat_room_id"
     t.index ["user_id"], name: "index_multi_user_messages_on_user_id"
   end
@@ -64,23 +63,23 @@ ActiveRecord::Schema.define(version: 2018_05_24_105833) do
   create_table "multi_user_private_chat_rooms", force: :cascade do |t|
     t.string "name"
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id"], name: "index_multi_user_private_chat_rooms_on_user_id"
   end
 
   create_table "private_chat_rooms", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "private_messages", force: :cascade do |t|
     t.text "content"
     t.bigint "user_id"
     t.bigint "private_chat_room_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["private_chat_room_id"], name: "index_private_messages_on_private_chat_room_id"
     t.index ["user_id"], name: "index_private_messages_on_user_id"
   end
@@ -89,22 +88,22 @@ ActiveRecord::Schema.define(version: 2018_05_24_105833) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "avatar"
     t.string "first_name"
     t.string "last_name"
     t.string "display_name"
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
+    t.datetime "confirmed_at", precision: nil
+    t.datetime "confirmation_sent_at", precision: nil
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
