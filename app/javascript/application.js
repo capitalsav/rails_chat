5eb697b0bae9c45a3c15b68972d72ebfc75b975a
Anton <capitalsav@users.noreply.github.com>
2023-09-12 18:10:09 +0300

    Migrated from webpacker to webpack

---
+++ app/javascript/application.js
@@ -0,0 +1,7 @@
+// Entry point for the build script in your package.json
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
+import './packs/actioncable_connect'
+import "./style/application.scss"
+
+console.log('Hello World from Webpack');
